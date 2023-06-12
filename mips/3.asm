2a3574a539640a4b56aef9bed54347e5d659b2e0
Shay Gali <86621168+ShayGali@users.noreply.github.com>
2023-06-12 18:42:26 +0300

    Update 3.asm

--- mips/3.asm
+++ mips/3.asm
@@ -12,16 +12,12 @@ addi		$t2, $0, 29		# $t2 = 29
 
 loop:
     bgt		$t1, $t2, done	# if $t1 > $t2 then goto done
-
     # save n into the address
     sw		$t1, 0($t0)		    # $t0 = $t1
-    
     # progress of the address
     addi		$t0, $t0, 4		# $t0 = $t0 + 4
-    
     # n+=2
     addi	$t1, $t1, 2			# $t1 = $t1 + 2
-    
-    j		loop				# jump to loop
 
+    j		loop				# jump to loop
 done:
